ec2548bb0800f8a723d4607d372287137dc19231
Michael <michael.shaohan@gmail.com>
2023-11-27 10:29:45 -0500

    housekeeper backend-frontend negotiation

--- src/components/HouseKeeper.tsx
+++ src/components/HouseKeeper.tsx
@@ -20,15 +20,16 @@ const HouseKeeper = () => {
   const checkRunStatus = async (threadId: string, runId: string) => {
     // console.log('checking status')
     try {
-      const statusResponse = await axios.get(`https://zck5pcxz76v6pdvzgnst3fntxa0yjqzv.lambda-url.ca-central-1.on.aws//api/threads/${threadId}/runs/${runId}`);
+      const statusResponse = await axios.get(`https://y75d43vkhohk37anophz77fo5m0wpzsu.lambda-url.ca-central-1.on.aws/api/threads/${threadId}/runs/${runId}`);
+      console.log('status is ' + statusResponse.data.status)
+      console.log(runId + " " + threadId)
       if (statusResponse.data.status === 'completed') {
-        console.log('status is ' + statusResponse.data.status)
         setStatus(statusResponse.data.status);
         fetchThreadMessages(threadId);
         return;
       } else {
         // Optionally, recheck after a delay if the run is not completed
-        setTimeout(() => checkRunStatus(threadId, runId), 1000);
+        setTimeout(() => checkRunStatus(threadId, runId), 2000);
       }
     } catch (error) {
       console.error('Error in checking run status:', error);
@@ -38,7 +39,7 @@ const HouseKeeper = () => {
   // Function to fetch all messages from a thread
   const fetchThreadMessages = async (threadId: string) => {
     try {
-      const messagesResponse = await axios.get(`https://zck5pcxz76v6pdvzgnst3fntxa0yjqzv.lambda-url.ca-central-1.on.aws//api/threads/${threadId}`);
+      const messagesResponse = await axios.get(`https://y75d43vkhohk37anophz77fo5m0wpzsu.lambda-url.ca-central-1.on.aws/api/threads/${threadId}`);
       const messages = messagesResponse.data.messages[0].content;
       setResponseMessage(messages)
       // setResponseMessage(messages.map(msg => msg.content).join('\n'));
@@ -50,7 +51,7 @@ const HouseKeeper = () => {
   // Function to send user input to the thread
   const sendUserInput = async () => {
     try {
-      const response = await axios.post(`https://zck5pcxz76v6pdvzgnst3fntxa0yjqzv.lambda-url.ca-central-1.on.aws//api/threads/${threadId}`, {
+      const response = await axios.post(`https://y75d43vkhohk37anophz77fo5m0wpzsu.lambda-url.ca-central-1.on.aws/api/threads/${threadId}`, {
         content: userInput
       });
       setUserInput(''); // Clear the input field
@@ -61,13 +62,16 @@ const HouseKeeper = () => {
     }
   };
 
-
+//https://y75d43vkhohk37anophz77fo5m0wpzsu.lambda-url.ca-central-1.on.aws/
   React.useEffect(() => {
     const initializeThread = async () => {
       try {
-        const response = await axios.post('https://zck5pcxz76v6pdvzgnst3fntxa0yjqzv.lambda-url.ca-central-1.on.aws//api/new');
+        const response = await axios.post('https://y75d43vkhohk37anophz77fo5m0wpzsu.lambda-url.ca-central-1.on.aws/api/new');
+        console.log(response)
         const newTID = response.data.thread_id;
-        const newRID = response.data.run_id
+        const newRID = response.data.run_id;
+        localStorage.setItem("thread_id", newTID);
+        localStorage.setItem("run_id", newRID);
         setThreadId(newTID);
         setRunId(newRID);
         // console.log("thread_id: " + threadId + " run_id: " + runId);
@@ -77,13 +81,23 @@ const HouseKeeper = () => {
       }
     };
 
-    initializeThread()
+    //only initialize a new thread at first visit to safe money
+    if(localStorage.getItem("thread_id") === null && localStorage.getItem("run_id") === null){
+      initializeThread()
+    } else {
+      //@ts-expect-error check already performed above
+      setThreadId(localStorage.getItem("thread_id"));
+      //@ts-expect-error check already performed above
+      setRunId(localStorage.getItem("run_id"))
+    }
   }, []);
 
 
   const handleSubmit:React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
-    sendUserInput();
+    if(userInput !== ""){
+      sendUserInput();
+    }
   }
 
   //API Functions end here
