126f48e54b2bd12959899b8d3e606fd4db6df43b
Michael <michael.shaohan@gmail.com>
2023-12-05 23:44:50 -0500

    added audio player

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
-import Fade from 'react-bootstrap/Fade';
+import { Fade } from 'react-bootstrap';
 import '../stylesheets/App.css';
 import Header from './Header.tsx';
 import Body from './Body.tsx';
 import HouseKeeper from './HouseKeeper.tsx';
 import IndexPage from './threeCanvas/pages/IndexPage/IndexPage.tsx';
+import AudioPlayer from './AudioPlayer.tsx'
+import InitPopUp from './InitPopUp.tsx';
 
 function App() {
   const [currentPage, setCurrentPage] = useState(0);
   const [scrollReady, setScrollReady] = useState(true);
+  const [playPause, setPlaypause] = useState(false)
+  const [showPopup, setShowPopup] = useState(false);
   const totalPages = 5;
+  const fadeInOut = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1, transition: { duration: 2, delay: 0 } },
+    exit: { opacity: 0, transition: { duration: 0.5 } }
+  };
 
   // Determine the model based on currentPage
   const getModelForPage = (page: number) => {
@@ -54,25 +63,48 @@ function App() {
     pageHandler(scrollDown);
   };
 
+  const handleEnableAudio = () => {
+    setPlaypause(true)
+    handleClosePopup()
+  };
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
   useEffect(() => {
     window.addEventListener('wheel', handleWheel, { passive: false });
     return () => window.removeEventListener('wheel', handleWheel);
   }, [scrollReady]);
 
-  console.log(currentPage);
+  useEffect(() => {
+    setShowPopup(true)
+  }, []);
 
+  console.log(currentPage);
   return (
     <Container className="Window p-0" fluid>
+      <Fade in={showPopup}>
+        <Container className="InitPopUp p-0" fluid>
+          <InitPopUp onEnableAudio={handleEnableAudio} onClose={handleClosePopup} />
+        </Container>
+      </Fade>
+
       <Container className="IndexPage p-0" fluid>
         <IndexPage name={model.name} time={model.time}/>
       </Container>
+      <Container className="AudioPlayer p-0 hidden" fluid>
+        <AudioPlayer playPause={playPause}/>
+      </Container>
+
       <Fade in={currentPage === 0}>
-        <Container className={`Header p-0 ${currentPage !== 0 ? 'hidden' : ''}`} fluid>
+        <Container className={`Header p-0 ${currentPage === 0 ? '' : 'hidden'}`} fluid>
           <Header />
         </Container>
       </Fade>
+
       <Fade in={currentPage === 1}>
-        <Container className={`Body p-0 ${currentPage !== 1 ? 'hidden' : ''}`} fluid>
+        <Container className={`HouseKeeper p-0 ${currentPage === 1 ? '' : 'hidden'}`} fluid>
           <HouseKeeper />
         </Container>
       </Fade>
